71da4c2a4813eb56849349d370e3bc29155c2b06
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-28 18:07:17 +0200

    v2: add WIP api-design

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,7 +43,7 @@ export default defineUserConfig({
       {
         text: "API v2 Reference",
         collapsible: true,
-        children: ["/api/v2/check-domain.md", "/api/v2/check-domain-details.md", "/api/v2/submit-domain.md"]
+        children: ["/api/v2/api-design.md", "/api/v2/check-domain.md", "/api/v2/check-domain-details.md", "/api/v2/submit-domain.md"]
       },
       {
         text: "Integrations",
